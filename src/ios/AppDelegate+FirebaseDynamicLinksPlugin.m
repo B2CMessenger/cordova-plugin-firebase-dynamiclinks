c0e8e1ee7c55695223ad4458a793270048064b19
Vladimir Rubanyuk <prestonviewer@gmail.com>
2020-04-07 12:15:59 +0300

    make it possible to apply Firebase Deferred Dynamic Links on first app launch after install process
    Sources used:
    - https://github.com/Chuckv01/cordova-plugin-firebase-dynamiclinks/pull/1
    - https://github.com/PDLMobileApps/cordova-plugin-firebase-dynamiclinks/commit/441631cb2113298df072fe154e005e292bcea16f

--- src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
+++ src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
@@ -4,6 +4,7 @@
 
 
 @implementation AppDelegate (FirebaseDynamicLinksPlugin)
+static NSString *const CUSTOM_URL_PREFIX_TO_IGNORE = @"/__/auth/callback";
 
 + (void)load {
     static dispatch_once_t onceToken;
@@ -11,6 +12,7 @@
     dispatch_once(&onceToken, ^{
         [self swizzleMethod:@selector(application:openURL:options:)];
         [self swizzleMethod:@selector(application:continueUserActivity:restorationHandler:)];
+        [self swizzleMethod:@selector(application:didFinishLaunchingWithOptions:)];
     });
 }
 
@@ -46,6 +48,40 @@
     return handled;
 }
 
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<NSString *, id> *)options {
+    return [self application:app
+                     openURL:url
+           sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                  annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+  FIRDynamicLink *dynamicLink = [[FIRDynamicLinks dynamicLinks] dynamicLinkFromCustomSchemeURL:url];
+
+    FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
+    if (dynamicLink) {
+        BOOL validDynamicLink = dynamicLink.url && ![dynamicLink.url.path hasPrefix:CUSTOM_URL_PREFIX_TO_IGNORE];
+        if (validDynamicLink) {
+            [dl postDynamicLink:dynamicLink];
+            return YES;
+        } else {
+            // Dynamic link has empty deep link. This situation will happens if
+            // Firebase Dynamic Links iOS SDK tried to retrieve pending dynamic link,
+            // but pending link is not available for this device/App combination.
+            // At this point you may display default onboarding view.
+        }
+    }
+    return [super application: application
+                      openURL:url
+            sourceApplication:sourceApplication
+                   annotation:annotation];
+}
+
 - (BOOL)default_application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
     return FALSE;
 }
@@ -53,11 +89,12 @@
 - (BOOL)swizzled_application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
     // always call original method implementation first
     BOOL handled = [self swizzled_application:app continueUserActivity:userActivity restorationHandler:restorationHandler];
-    FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
+
     // handle firebase dynamic link
     return [[FIRDynamicLinks dynamicLinks]
         handleUniversalLink:userActivity.webpageURL
         completion:^(FIRDynamicLink * _Nullable dynamicLink, NSError * _Nullable error) {
+            FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
             // Try this method as some dynamic links are not recognize by handleUniversalLink
             // ISSUE: https://github.com/firebase/firebase-ios-sdk/issues/743
             dynamicLink = dynamicLink ? dynamicLink
@@ -70,4 +107,24 @@
         }] || handled;
 }
 
+// [START didfinishlaunching]
+- (BOOL)default_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey, id> *)launchOptions {
+    return FALSE;
+}
+- (BOOL)swizzled_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey, id> *)launchOptions {
+    // always call original method implementation first
+    [self swizzled_application:application didFinishLaunchingWithOptions:launchOptions];
+    [FIROptions defaultOptions].deepLinkURLScheme = [[NSBundle mainBundle] bundleIdentifier];
+
+    if (![FIRApp defaultApp]) {
+        [FIRApp configure];
+    }
+    NSDictionary *userActivityDictionary = [launchOptions objectForKey:UIApplicationLaunchOptionsUserActivityDictionaryKey];
+
+    if (userActivityDictionary) {
+        // Continue activity here
+        return YES;
+    }
+}
+// [END didfinishlaunching]
 @end
